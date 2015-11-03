c91e6f0fde266dc8e953d2e4d5b7d3ad974350a0
Wojciech A. Koszek <wojciech@koszek.com>
2015-11-03 00:40:39 -0800

    Set upstream branch.

--- run.sh
+++ run.sh
@@ -21,7 +21,10 @@ if [ $? -eq 0 ]; then
 	git clone git@github.com:wkoszek/test.git
 	cd test
 	git checkout -B gh-pages
+	git branch --set-upstream-to origin/gh-pages
+
 	mv ../../output.html .
+
 	git add output.html
 	git commit -m "New stuff `date`" output.html
 	git push origin gh-pages
