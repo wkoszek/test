0727531bd98c9bb7337527b4a11f07048d70a395
Wojciech A. Koszek <wojciech@koszek.com>
2015-11-03 00:25:29 -0800

    Fix the run  script.

--- run.sh
+++ run.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
+set -x
+
 (
 	date
 	./simple.sh
 ) > output.html
 
 if [ $? -eq 0 ]; then
+	sh -i -c ssh-agent > _
+	source _
 	mkdir tmp
 	cd tmp
 	git clone git@github.com:wkoszek/test.git
 	cd test
 	git checkout -B gh-pages
-	mv ../output.html .
+	mv ../../output.html .
 	git add output.html
 	git commit -m "New stuff `date`" output.html
 	git push origin gh-pages
